851b555e8c0e88e17c51614a82ad76685e3a9562
SamPan96 <samuelpanzieri@yahoo.it>
2023-09-13 13:26:28 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://spotify-rooms-backend.herokuapp.com"
+const baseUrl = "https://spotify-rooms-backend.onrender.com"
 // const baseUrl = "http://127.0.0.1:8000"
 export default baseUrl
